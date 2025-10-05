2af5ae721fe7cc6c002b2b945f4647b212177ee7
Arunnil S Pandey <arunilpandey224@gmail.com>
2025-10-05 10:24:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6409199899-cb001",
+  "appId": "1:108170289927:web:8a8e8ea564080262b31ba2",
+  "apiKey": "AIzaSyCTio6VHtkSnx_QRq0-x0dcY8vpcD_1gvg",
+  "authDomain": "studio-6409199899-cb001.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "108170289927"
+};
